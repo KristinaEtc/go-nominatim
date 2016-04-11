95da551bcfccd75b42d76c280d2e5369f9f20f11
Kristina <isitiriss@gmail.com>
2016-04-11 13:26:26 +0300

    added example config file for db (config.json.example)

--- cli/main.go
+++ cli/main.go
@@ -3,55 +3,95 @@ package main
 import (
 	"Nominatim/lib"
 	"database/sql"
+	"encoding/json"
 	"flag"
 	_ "github.com/lib/pq"
 	"log"
+	"os"
 	//"strconv"
 )
+
+var configFile = "config.json"
+
+type ConfigDB struct {
+	DBname, Host, User, Password string
+}
+
+type LocationParams struct {
+	format         string
+	lat            float64
+	lon            float64
+	zoom           int
+	addressDetails bool
+	sqlOpenStr     string
+	config         ConfigDB
+	db             *sql.DB
+}
 
 //var baseURL string = "http://192.168.240.105/nominatim/"
 
 //"-c nominatim/reverse?format=xml&lat=53.913658&lon=27.600286&zoom=18&addressdetails=1"
 
-func getParams() (format string, lat, lon float64, zoom int, addressDetails bool, sqlOpenStr string) {
+func (l *LocationParams) getParams() {
 
-	flag.Parsed()
-
-	flag.StringVar(&format, "f", "json", "format")
-	flag.Float64Var(&lat, "a", 53.902238, "lat")
-	flag.Float64Var(&lon, "b", 27.561916, "log")
-	flag.IntVar(&zoom, "z", 18, "zoom")
-	flag.BoolVar(&addressDetails, "d", false, "addressDetails")
-	flag.StringVar(&sqlOpenStr, "s", "dbname=nominatim", "sqlOpenStr")
+	flag.StringVar(&(l.format), "f", "json", "format")
+	flag.Float64Var(&l.lat, "a", 53.902238, "lat")
+	flag.Float64Var(&l.lon, "b", 27.561916, "log")
+	flag.IntVar(&l.zoom, "z", 18, "zoom")
+	flag.BoolVar(&l.addressDetails, "d", false, "addressDetails")
 
 	flag.Parse()
-
-	//fmt.Println(format, lat, lon, zoom, addressDetails)
+	//flag.Parsed()
 
 	return
 }
 
-func getDB(sqlOpenStr string) sql.DB {
+func (l *LocationParams) getDB(sqlOpenStr string) {
 	db, err := sql.Open("postgres", sqlOpenStr)
 	if err != nil {
 		log.Fatal(err) ///qqqqqqq
 
 	}
-	return *db
+	l.db = db
+}
+
+func (l *LocationParams) configurateDB() {
+
+	file, err := os.Open(configFile)
+	if err != nil {
+		log.Printf("No configurate file")
+	} else {
+		decoder := json.NewDecoder(file)
+		configuration := ConfigDB{}
+		err := decoder.Decode(&configuration)
+		if err != nil {
+			log.Println("error: ", err)
+		}
+		l.config = configuration
+	}
 }
 
 func main() {
 
-	format, lat, lon, zoom, addressDetails, sqlOpenStr := getParams()
-	log.Print(format, lat, lon, zoom, addressDetails, sqlOpenStr)
+	l := LocationParams{}
 
-	db := getDB(sqlOpenStr)
+	l.configurateDB()
+	l.getParams()
 
-	oReverseGeocode := Nominatim.NewReverseGeocode(db)
+	//log.Print(l)
+
+	sqlOpenStr := "dbname=" + l.config.DBname +
+		" host=" + l.config.Host +
+		" user=" + l.config.User +
+		" password=" + l.config.Password
+
+	l.getDB(sqlOpenStr)
+
+	oReverseGeocode := Nominatim.NewReverseGeocode(*l.db)
 	//oReverseGeocode.SetLanguagePreference()
-	oReverseGeocode.SetIncludeAddeessDetails(addressDetails)
-	oReverseGeocode.SetZoom(zoom)
-	oReverseGeocode.SetLocation(lat, lon)
+	oReverseGeocode.SetIncludeAddeessDetails(l.addressDetails)
+	oReverseGeocode.SetZoom(l.zoom)
+	oReverseGeocode.SetLocation(l.lat, l.lon)
 
 	oReverseGeocode.Lookup()
 
