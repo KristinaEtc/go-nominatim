0eba49a41d12e1b5594a3195d3c915c26ecd4200
Kristina <isitiriss@gmail.com>
2016-06-01 19:55:06 +0300

    Added fileWriter object

--- lib/utils/fileproc/fileproc.go
+++ lib/utils/fileproc/fileproc.go
@@ -8,7 +8,20 @@ import (
 type FileScanner struct {
 	File    *os.File
 	Scanner *bufio.Scanner
-	Reader  *bufio.Reader
+}
+
+type FileWriter struct {
+	File   *os.File
+	Writer *bufio.Writer
+}
+
+func NewFileWriter(fileName string) (*FileWriter, error) {
+	tmpFile, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0660)
+	if err != nil {
+		return nil, err
+	}
+	fs := FileWriter{File: tmpFile}
+	return &fs, nil
 }
 
 func NewFileScanner(fileName string) (*FileScanner, error) {
@@ -24,10 +37,22 @@ func (f *FileScanner) Close() error {
 	return f.File.Close()
 }
 
+func (f *FileWriter) Close() error {
+	return f.File.Close()
+}
+
 func (f *FileScanner) GetScanner() *bufio.Scanner {
 	if f.Scanner == nil {
 		f.Scanner = bufio.NewScanner(f.File)
 		//f.Scanner.Split(bufio.ScanLines)
 	}
 	return f.Scanner
+}
+
+func (f *FileWriter) GetWriter() *bufio.Writer {
+	if f.Writer == nil {
+		f.Writer = bufio.NewWriter(f.File)
+		//f.Scanner.Split(bufio.ScanLines)
+	}
+	return f.Writer
 }
