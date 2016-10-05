8f29c21ecc2747663b586afab9f964f0d7163626
Kristina <isitiriss@gmail.com>
2016-10-05 15:16:53 +0300

    more working request to nominatim

--- worker/stomp/go-stomp-nominatim/go-stomp-nominatim.go
+++ worker/stomp/go-stomp-nominatim/go-stomp-nominatim.go
@@ -7,9 +7,9 @@ import (
 	"time"
 
 	//important: must execute first; do not move
+	"github.com/KristinaEtc/config"
 	_ "github.com/KristinaEtc/slflog"
 
-	"github.com/KristinaEtc/config"
 	"github.com/KristinaEtc/go-nominatim/lib"
 	"github.com/go-stomp/stomp"
 	_ "github.com/lib/pq"
@@ -176,6 +176,180 @@ type monitoringData struct {
 
 //--------------------------------------------------------------------------
 
+func main() {
+
+	log = slf.WithContext("go-stomp-nominatim.go")
+
+	//params := Params{}
+	config.ReadGlobalConfig(&globalOpt, "go-stomp-nominatim options")
+	initOptions()
+
+	subscribed := make(chan bool)
+	timeout := make(chan []byte)
+
+	log.Error("----------------------------------------------")
+
+	log.Infof("BuildDate=%s\n", BuildDate)
+	log.Infof("GitCommit=%s\n", GitCommit)
+	log.Infof("GitBranch=%s\n", GitBranch)
+	log.Infof("GitState=%s\n", GitState)
+	log.Infof("GitSummary=%s\n", GitSummary)
+	log.Infof("VERSION=%s\n", Version)
+
+	log.Info("Starting working...")
+
+	reverseGeocode, err := initReverseGeocode()
+	if err != nil {
+		log.WithCaller(slf.CallerShort).Error(err.Error())
+		os.Exit(1)
+	}
+	defer reverseGeocode.Close()
+
+	go runProcessLoop(reverseGeocode, subscribed, timeout)
+	<-subscribed
+
+	go sendStatus(timeout)
+
+	<-stop
+	<-stop
+}
+
+func initOptions() {
+
+	options = []func(*stomp.Conn) error{
+		stomp.ConnOpt.Login(globalOpt.ConnConf.ServerUser, globalOpt.ConnConf.ServerPassword),
+		stomp.ConnOpt.Host(globalOpt.ConnConf.ServerAddr),
+		stomp.ConnOpt.HeartBeatError(time.Second * time.Duration(globalOpt.ConnConf.HeartBeatError)),
+		stomp.ConnOpt.HeartBeat(time.Second*time.Duration(globalOpt.ConnConf.HeartBeat),
+			time.Second*time.Duration(globalOpt.ConnConf.HeartBeat)),
+	}
+}
+
+func initReverseGeocode() (*Nominatim.ReverseGeocode, error) {
+
+	log.Debug("initReverseGeocode")
+
+	sqlOpenStr := "dbname=" + globalOpt.NominatimDB.DBname +
+		" host=" + globalOpt.NominatimDB.Host +
+		" user=" + globalOpt.NominatimDB.User +
+		" password=" + globalOpt.NominatimDB.Password
+
+	log.WithCaller(slf.CallerShort).Debugf("sqlOpenStr=%s", sqlOpenStr)
+
+	reverseGeocode, err := Nominatim.NewReverseGeocode(sqlOpenStr)
+	if err != nil {
+		return nil, err
+	}
+	return reverseGeocode, nil
+}
+
+func runProcessLoop(reverseGeocode *Nominatim.ReverseGeocode, subscribed chan bool, timeToMonitoring chan []byte) {
+	defer func() {
+		stop <- true
+	}()
+
+	connSubsc, err := stomp.Dial("tcp", globalOpt.ConnConf.ServerAddr, options...)
+	if err != nil {
+		log.WithCaller(slf.CallerShort).Errorf("cannot connect to server (connSubsc): %s", err.Error())
+		return
+	}
+
+	connSend, err := stomp.Dial("tcp", globalOpt.ConnConf.ServerAddr, options...)
+	if err != nil {
+		log.WithCaller(slf.CallerShort).Errorf("cannot connect to server (connSend): %s", err.Error())
+		return
+	}
+
+	sub, err := connSubsc.Subscribe(globalOpt.QueueConf.QueueName, stomp.AckAuto)
+	if err != nil {
+		log.WithCaller(slf.CallerShort).Errorf("cannot subscribe to %s: %s",
+			globalOpt.QueueConf.QueueName, err.Error())
+		return
+	}
+
+	//subPrior, err := connSubsc.Subscribe(globalOpt.QueueConf.QueuePriorName, stomp.AckAuto)
+	//if err != nil {
+	//	log.WithCaller(slf.CallerShort).Errorf("cannot subscribe to %s: %s",
+	//		globalOpt.QueueConf.QueueName, err.Error())
+	//	return
+	//}
+
+	close(subscribed)
+
+	// init a struct with info for monitoring queque
+	data := initMonitoringData(connSend.GetConnInfo())
+
+	ticker := time.NewTicker(time.Duration(globalOpt.DiagnConf.TimeOut) * time.Second)
+	var ok bool
+	var msg *stomp.Message
+
+	// for requests/per sec; errors/per sec
+	var prevNumOfReq, prevNumOfErr, prevNumOfErrResp, prevNumOfSuccResp int64
+	//var queque string
+
+	for {
+		ok = false
+		select {
+		case msg, ok = <-sub.C:
+			//break
+		case <-ticker.C:
+			data.CurrentTime = time.Now().Format(time.RFC3339)
+			calculateSeverity(data)
+			calculateRatePerSec(data, &prevNumOfReq, &prevNumOfErr, &prevNumOfErrResp, &prevNumOfSuccResp)
+			log.Infof("data: %v", data)
+			b, err := json.Marshal(data)
+			if err != nil {
+				log.Error(err.Error())
+				continue
+			}
+			timeToMonitoring <- b
+			continue
+		}
+		start := time.Now()
+
+		if !ok {
+			log.Warn("msg, ok = <-sub.C: !ok")
+			data.ReconnectCount++
+			data.LastError = "Reconnect"
+			continue
+		}
+
+		data.Reqs++
+
+		reqJSON := msg.Body
+		var p Params
+		p.machineId = connSend.GetConnInfo()
+
+		replyJSON, whoToSent, errResp, err := p.locationSearch(reqJSON, reverseGeocode)
+		if err != nil {
+			log.WithCaller(slf.CallerShort).Errorf("Error: locationSearch %s; fullMsg=%v", err.Error(), msg)
+			data.ErrorCount++
+			data.LastError = err.Error()
+			continue
+		}
+		if errResp == true {
+			data.ErrResp++
+		} else {
+			data.SuccResp++
+		}
+
+		err = connSend.Send(globalOpt.ConnConf.QueueFormat+*whoToSent, "application/json;charset=utf-8",
+			[]byte(replyJSON), nil...)
+		if err != nil {
+			data.MachineAddr = connSend.GetConnInfo()
+			data.ErrorCount++
+			log.WithCaller(slf.CallerShort).Errorf("Failed to send to server %s", err)
+			time.Sleep(time.Second)
+			data.LastError = err.Error()
+			continue
+		}
+
+		elapsed := float64(time.Since(start)) / 1000.0 / 1000.0
+		//data.EMA = (data.EMA + elapsed) / 2
+		data.AverageRate = (1-globalOpt.DiagnConf.CoeffEMA)*data.AverageRate + globalOpt.DiagnConf.CoeffEMA*elapsed
+	}
+}
+
 func createErrResponse(err error) []byte {
 	respJSON := ErrorResponse{Type: "error", Message: err.Error()}
 
@@ -269,112 +443,6 @@ func getLocationJSON(data Nominatim.DataWithoutDetails) ([]byte, error) {
 	return dataJSON, nil
 }
 
-func runProcessLoop(reverseGeocode *Nominatim.ReverseGeocode, subscribed chan bool, timeToMonitoring chan []byte) {
-	defer func() {
-		stop <- true
-	}()
-
-	connSubsc, err := stomp.Dial("tcp", globalOpt.ConnConf.ServerAddr, options...)
-	if err != nil {
-		log.WithCaller(slf.CallerShort).Errorf("cannot connect to server (connSubsc): %s", err.Error())
-		return
-	}
-
-	connSend, err := stomp.Dial("tcp", globalOpt.ConnConf.ServerAddr, options...)
-	if err != nil {
-		log.WithCaller(slf.CallerShort).Errorf("cannot connect to server (connSend): %s", err.Error())
-		return
-	}
-
-	sub, err := connSubsc.Subscribe(globalOpt.QueueConf.QueueName, stomp.AckAuto)
-	if err != nil {
-		log.WithCaller(slf.CallerShort).Errorf("cannot subscribe to %s: %s",
-			globalOpt.QueueConf.QueueName, err.Error())
-		return
-	}
-
-	//subPrior, err := connSubsc.Subscribe(globalOpt.QueueConf.QueuePriorName, stomp.AckAuto)
-	//if err != nil {
-	//	log.WithCaller(slf.CallerShort).Errorf("cannot subscribe to %s: %s",
-	//		globalOpt.QueueConf.QueueName, err.Error())
-	//	return
-	//}
-
-	close(subscribed)
-
-	// init a struct with info for monitoring queque
-	data := initMonitoringData(connSend.GetConnInfo())
-
-	ticker := time.NewTicker(time.Duration(globalOpt.DiagnConf.TimeOut) * time.Second)
-	var ok bool
-	var msg *stomp.Message
-
-	// for requests/per sec; errors/per sec
-	var prevNumOfReq, prevNumOfErr, prevNumOfErrResp, prevNumOfSuccResp int64
-	//var queque string
-
-	for {
-		ok = false
-		select {
-		case msg, ok = <-sub.C:
-			//break
-		case <-ticker.C:
-			data.CurrentTime = time.Now().Format(time.RFC3339)
-			data.Reqs++
-			calculateSeverity(data)
-			calculateRatePerSec(data, &prevNumOfReq, &prevNumOfErr, &prevNumOfErrResp, &prevNumOfSuccResp)
-			log.Infof("data: %v", data)
-			b, err := json.Marshal(data)
-			if err != nil {
-				log.Error(err.Error())
-				continue
-			}
-			timeToMonitoring <- b
-			//continue
-		}
-		start := time.Now()
-
-		if !ok {
-			log.Warn("msg, ok = <-sub.C: !ok")
-			data.ReconnectCount++
-			data.LastError = "Reconnect"
-			continue
-		}
-
-		reqJSON := msg.Body
-		var p Params
-		p.machineId = connSend.GetConnInfo()
-
-		replyJSON, whoToSent, errResp, err := p.locationSearch(reqJSON, reverseGeocode)
-		if err != nil {
-			log.WithCaller(slf.CallerShort).Errorf("Error: locationSearch %s; fullMsg=%v", err.Error(), msg)
-			data.ErrorCount++
-			data.LastError = err.Error()
-			continue
-		}
-		if errResp == true {
-			data.ErrResp++
-		} else {
-			data.SuccResp++
-		}
-
-		err = connSend.Send(globalOpt.ConnConf.QueueFormat+*whoToSent, "application/json;charset=utf-8",
-			[]byte(replyJSON), nil...)
-		if err != nil {
-			data.MachineAddr = connSend.GetConnInfo()
-			data.ErrorCount++
-			log.WithCaller(slf.CallerShort).Errorf("Failed to send to server %s", err)
-			time.Sleep(time.Second)
-			data.LastError = err.Error()
-			continue
-		}
-
-		elapsed := float64(time.Since(start)) / 1000.0 / 1000.0
-		//data.EMA = (data.EMA + elapsed) / 2
-		data.AverageRate = (1-globalOpt.DiagnConf.CoeffEMA)*data.AverageRate + globalOpt.DiagnConf.CoeffEMA*elapsed
-	}
-}
-
 //calculateRatePerSec(data, &prevNumOfReq, &prevNumOfErr, &prevNumOfErrResp)
 func calculateRatePerSec(data monitoringData, prevNumOfReq *int64, prevNumOfErr *int64, prevNumOfErrResp *int64, prevSuccR *int64) {
 
@@ -383,7 +451,11 @@ func calculateRatePerSec(data monitoringData, prevNumOfReq *int64, prevNumOfErr 
 		return
 	}
 
+	//log.Warnf(" BEFORE: prevNumOfReq=%dz, prevNumOfErr=%d, prevNumOfErrResp=%d, prevSuccR=%d", *prevNumOfReq, *prevNumOfErr, *prevNumOfErrResp, *prevSuccR)
+	//log.Warnf(" BEFORE: data.Reqs=%dz, data.ErrorCount=%d, data.ErrResp=%d, data.SuccResp=%d", data.Reqs, data.ErrorCount, data.ErrResp, data.SuccResp)
+
 	period := int64(globalOpt.DiagnConf.TimeOut)
+
 	data.RequestRate = (data.Reqs - *(prevNumOfReq)) / period
 	data.ErrorRate = (data.ErrorCount - *(prevNumOfErr)) / period
 	data.ErrorRespRate = (data.ErrResp - *(prevNumOfErrResp)) / period
@@ -392,104 +464,18 @@ func calculateRatePerSec(data monitoringData, prevNumOfReq *int64, prevNumOfErr 
 	prevSuccResp := *prevNumOfReq - *prevNumOfErr - *prevNumOfErrResp
 	if prevSuccResp != *prevSuccR {
 		//for first time; to check that all right
-		log.Warnf("Wrong success responce calculating %s %s", prevSuccResp, *prevSuccR)
+		log.Warnf("Wrong success responce calculating %d %d", prevSuccResp, *prevSuccR)
 	}
 	data.SuccessRespRate = (data.SuccResp - prevSuccResp) / period
+
+	//log.Warnf("data.RequestRate =%d, data.ErrorRate=%d, data.ErrorRespRate=%d, data.SuccessRespRate=%d", data.RequestRate, data.ErrorRate, data.ErrorRespRate, data.SuccessRespRate)
 
 	*prevNumOfErr = data.ErrorCount
 	*prevNumOfErrResp = data.ErrResp
 	*prevNumOfReq = data.Reqs
 	*prevSuccR = data.SuccResp
-}
 
-func sendStatus(timeToMonitoring chan []byte) {
-
-	defer func() {
-		stop <- true
-	}()
-
-	connSend, err := stomp.Dial("tcp", globalOpt.ConnConf.ServerAddr, options...)
-	if err != nil {
-		log.Errorf("cannot connect to server %s", err.Error())
-		return
-	}
-	for {
-		select {
-		case data := <-timeToMonitoring:
-
-			err = connSend.Send(globalOpt.DiagnConf.TopicName, "application/json", data, nil...)
-			if err != nil {
-				log.Errorf("Error %s", err.Error())
-				continue
-			}
-
-		}
-	}
-}
-
-func initOptions() {
-
-	options = []func(*stomp.Conn) error{
-		stomp.ConnOpt.Login(globalOpt.ConnConf.ServerUser, globalOpt.ConnConf.ServerPassword),
-		stomp.ConnOpt.Host(globalOpt.ConnConf.ServerAddr),
-		stomp.ConnOpt.HeartBeatError(time.Second * time.Duration(globalOpt.ConnConf.HeartBeatError)),
-		stomp.ConnOpt.HeartBeat(time.Second*time.Duration(globalOpt.ConnConf.HeartBeat),
-			time.Second*time.Duration(globalOpt.ConnConf.HeartBeat)),
-	}
-}
-
-func main() {
-
-	log = slf.WithContext("go-stomp-nominatim.go")
-
-	//params := Params{}
-	config.ReadGlobalConfig(&globalOpt, "go-stomp-nominatim options")
-	initOptions()
-
-	subscribed := make(chan bool)
-	timeout := make(chan []byte)
-
-	log.Error("----------------------------------------------")
-
-	log.Infof("BuildDate=%s\n", BuildDate)
-	log.Infof("GitCommit=%s\n", GitCommit)
-	log.Infof("GitBranch=%s\n", GitBranch)
-	log.Infof("GitState=%s\n", GitState)
-	log.Infof("GitSummary=%s\n", GitSummary)
-	log.Infof("VERSION=%s\n", Version)
-
-	log.Info("Starting working...")
-
-	reverseGeocode, err := initReverseGeocode()
-	if err != nil {
-		log.WithCaller(slf.CallerShort).Error(err.Error())
-		os.Exit(1)
-	}
-	defer reverseGeocode.Close()
-
-	go runProcessLoop(reverseGeocode, subscribed, timeout)
-	<-subscribed
-
-	go sendStatus(timeout)
-
-	<-stop
-	<-stop
-}
-
-func initReverseGeocode() (*Nominatim.ReverseGeocode, error) {
-
-	sqlOpenStr := "dbname=" + globalOpt.NominatimDB.DBname +
-		" host=" + globalOpt.NominatimDB.Host +
-		" user=" + globalOpt.NominatimDB.User +
-		" password=" + globalOpt.NominatimDB.Password
-
-	log.WithCaller(slf.CallerShort).Debugf("sqlOpenStr=%s", sqlOpenStr)
-
-	reverseGeocode, err := Nominatim.NewReverseGeocode(sqlOpenStr)
-	if err != nil {
-		return nil, err
-	}
-	return reverseGeocode, nil
+	//log.Warnf("AFTER: prevNumOfReq=%d, prevNumOfErr=%d, prevNumOfErrResp=%d, prevSuccR=%d", *prevNumOfReq, *prevNumOfErr, *prevNumOfErrResp, *prevSuccR)
 }
 
 func calculateSeverity(data monitoringData) {
@@ -532,4 +518,29 @@ func initMonitoringData(machineAddr string) monitoringData {
 		ErrorRate:    0,
 	}
 	return data
+}
+
+func sendStatus(timeToMonitoring chan []byte) {
+
+	defer func() {
+		stop <- true
+	}()
+
+	connSend, err := stomp.Dial("tcp", globalOpt.ConnConf.ServerAddr, options...)
+	if err != nil {
+		log.Errorf("cannot connect to server %s", err.Error())
+		return
+	}
+	for {
+		select {
+		case data := <-timeToMonitoring:
+
+			err = connSend.Send(globalOpt.DiagnConf.TopicName, "application/json", data, nil...)
+			if err != nil {
+				log.Errorf("Error %s", err.Error())
+				continue
+			}
+
+		}
+	}
 }
