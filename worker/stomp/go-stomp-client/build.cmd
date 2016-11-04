01f7ba16a23b3bc9d6b2aade44ad2b8af3ec24a5
Kristina <isitiriss@gmail.com>
2016-11-04 18:11:49 +0300

    deleted worker directory

--- worker/stomp/go-stomp-client/build.cmd
+++
@@ -1,1 +0,0 @@
-go build -ldflags "-X github.com/KristinaEtc/config.CallerInfo=true" go-stomp-client.go 
--- worker/stomp/go-stomp-client/run.cmd
+++
@@ -1,1 +0,0 @@
-go-stomp-client.exe --Verbose=1 --Global.ServerAddr=192.168.240.60:61614 --Global.TestFile=test_small.csv --Global.ClientID=AzimutTestClient --Global.MessageDumpInterval=1
